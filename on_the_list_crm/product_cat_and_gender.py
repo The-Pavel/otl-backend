02e1be31f949d2cf3dd1518747be94fd853b46f7
Pavel Liser <liser.pavel@gmail.com>
2022-01-11 18:12:09 +0800

    removed notebooks and replaced tf with tf-cpu

--- on_the_list_crm/product_cat_and_gender.py
+++ on_the_list_crm/product_cat_and_gender.py
@@ -4,7 +4,7 @@ import pytz
 import pickle
 
 
-from on_the_list_crm.cleansing_dataset import get_raw_data, BasicCleaner
+#from on_the_list_crm.cleansing_dataset import get_raw_data, BasicCleaner
 
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.preprocessing.text import Tokenizer
